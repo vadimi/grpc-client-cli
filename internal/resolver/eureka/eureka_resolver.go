e425ddf921a69936f641aff7432e55a1774434ab
Justin Haygood <jhaygood86@gmail.com>
2020-05-04 22:37:43 -0400

    Feature/eureka alternative ports (#8)
    * Add Eureka Resolver
    
    Add Eureka Resolver
    
    * Update to support some additional URL options
    
    Update to support additional URL options
    
    * Use net/url to construct Eureka URI
    
    Use net/url to construct Eureka URI
    
    * Fix issues found by @vadimi
    
    Fix issues identified by Vadim
    
    * Switch to use ReportError
    
    Use ReportError when there's an error versus fatal logs
    
    * Set default port for Eureka to 8761
    
    If no default port exists, use port 8761
    
    * Not all services host grpc on default port
    
    Allow grpc services to be hosted on alternative ports than the default port published in Eureka, i.e., hybrid REST/gRPC apps.

--- internal/resolver/eureka/eureka_resolver.go
+++ internal/resolver/eureka/eureka_resolver.go
@@ -97,7 +97,18 @@ func (d *eurekaResolver) ResolveNow(resolver.ResolveNowOptions) {
 		var newAddrs []resolver.Address = make([]resolver.Address, 0, len(application.Instances))
 
 		for _, instance := range application.Instances {
-			addr := instance.IpAddr + ":" + strconv.Itoa(application.Instances[0].Port.Port)
+
+			port := strconv.Itoa(instance.Port.Port)
+
+			if val, ok := instance.Metadata.Map["grpc"]; ok {
+				port = val
+			}
+
+			if val, ok := instance.Metadata.Map["grpc.port"]; ok {
+				port = val
+			}
+
+			addr := instance.IpAddr + ":" + port
 			newAddrs = append(newAddrs, resolver.Address{Addr: addr})
 		}
 
